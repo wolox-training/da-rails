431e47c488ee51fb06e04dab5d128cf4c7eb515c
darwin araque <ingdarwinaraque@gmail.com>
2018-10-18 10:56:18 -0500

    This commit includes a modification for use the devise auth token gem, it allows to send a request to /auth/sign_in with email & pswd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  # before_action :authenticate_user!
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+
+include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+        before_save -> { skip_confirmation! }
 end
