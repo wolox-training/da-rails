ee5bb399c3697ab598af2dc6f1de5ca5b788a6fd
darwin araque <ingdarwinaraque@gmail.com>
2018-10-18 13:00:58 -0500

    This commit allows to persist a new object with the name Book

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_184746) do
+ActiveRecord::Schema.define(version: 2018_10_18_175306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "genero", default: "", null: false
+    t.string "autor", default: "", null: false
+    t.string "image", default: "", null: false
+    t.string "titulo", default: "", null: false
+    t.string "editor", default: "", null: false
+    t.string "anio", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
