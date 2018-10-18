45da26d3b6af60e00c704f13925ffda847c16d10
darwin araque <ingdarwinaraque@gmail.com>
2018-10-18 15:41:06 -0500

    Correctins for idents lines migration books

--- db/migrate/20181018175306_create_books.rb
+++ db/migrate/20181018175306_create_books.rb
@@ -1,19 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :genero,             null: false, default: ""
-      
-      t.string :autor,              null: false, default: ""
-      
-      t.string :image,              null: false, default: ""
-      
-      t.string :titulo,             null: false, default: ""
-      
-      t.string :editor,             null: false, default: ""
-      
-      t.string :anio,               null: false, default: ""
-      
-
+      t.string :genero, null: false, default: ""
+      t.string :autor, null: false, default: ""
+      t.string :image, null: false, default: ""
+      t.string :titulo, null: false, default: ""
+      t.string :editor, null: false, default: ""
+      t.string :anio, null: false, default: ""
       t.timestamps
     end
   end
