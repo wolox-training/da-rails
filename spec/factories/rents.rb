668742b94b05c677f5e47bb2ebbf1e27b3a484bb
darwin araque <ingdarwinaraque@gmail.com>
2018-10-19 16:35:18 -0500

    Start to develop rent model

--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -4,5 +4,5 @@ FactoryBot.define do
     association :book, factory: :book
     init_date { Faker::Time.between(5.days.ago, Time.now.utc) }
     end_date { Faker::Time.between(2.days.ago, Time.now.utc) }
-  end
+  end 
 end
