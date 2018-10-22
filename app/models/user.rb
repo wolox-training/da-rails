662f2d8d92901cb92bf1660ba281ee1bc6872e6e
darwin araque <ingdarwinaraque@gmail.com>
2018-10-22 12:09:45 -0500

    Modifications to rubocop file and anothers files to run bundle rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-
-include DeviseTokenAuth::Concerns::User
+  include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-        before_save -> { skip_confirmation! }
+
+  before_save -> { skip_confirmation! }
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,53 +2,54 @@ require 'rails_helper'
 require 'shoulda/matchers'
 
 describe Book, type: :model do
-    it do
-        should validate_presence_of(:gender)
-    end
-    it do
-        should validate_presence_of(:author)
-    end
-    it do
-        should validate_presence_of(:image)
-    end
-    it do
-        should validate_presence_of(:title)
-    end
-    it do
-        should validate_presence_of(:editor)
-    end
-    it do
-        should validate_presence_of(:year)
+  it do
+    should validate_presence_of(:gender)
+  end
+  it do
+    should validate_presence_of(:author)
+  end
+  it do
+    should validate_presence_of(:image)
+  end
+  it do
+    should validate_presence_of(:title)
+  end
+  it do
+    should validate_presence_of(:editor)
+  end
+  it do
+    should validate_presence_of(:year)
+  end
+
+  subject(:book) do
+    Book.new(gender: gender, author: author, image: image,
+             title: title, editor: editor, year: year)
+  end
+
+  let(:gender) { Faker::Food.dish }
+  let(:author) { Faker::Simpsons.character }
+  let(:image) { Faker::Simpsons.quote }
+  let(:title) { Faker::Food.dish }
+  let(:editor) { Faker::Simpsons.location }
+  let(:year) { '1993' }
+
+  it do
+    is_expected.to be_valid
+  end
+
+  describe '#create' do
+    context 'When the gender is nil' do
+      let(:gender) { nil }
+      it do
+        is_expected.to be_invalid
+      end
     end
 
-    subject(:book) do
-        Book.new(gender: gender, author: author, image: image, title: title, editor: editor, year: year)
+    context 'When the author is nil' do
+      let(:author) { nil }
+      it do
+        is_expected.to be_invalid
+      end
     end
-
-    let(:gender) { Faker::Food.dish }
-    let(:author) { Faker::Simpsons.character }
-    let(:image) { Faker::Simpsons.quote }
-    let(:title) { Faker::Food.dish }
-    let(:editor) { Faker::Simpsons.location }
-    let(:year) { '1993' }
-
-    it do
-        is_expected.to be_valid
-    end
-
-    describe '#create' do
-        context 'When the gender is nil' do
-            let(:gender) {nil}
-            it do
-                is_expected.to be_invalid
-            end
-        end
-
-        context 'When the author is nil' do
-            let(:author) {nil}
-            it do
-                is_expected.to be_invalid
-            end
-        end
-    end
+  end
 end
