be2ab33ee1122b44d5365349b6c40db5f8e390e2
darwin araque <ingdarwinaraque@gmail.com>
2018-10-23 15:40:04 -0500

    Salve changes for attemp a bug on rspec

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -2,7 +2,23 @@ require 'rails_helper'
 
 module Api
   module V1
-    describe BooksController do
+    describe BooksController type: :controller do
+      describe 'GET index' do
+        context 'When fetching all books' do
+          let!(:books) {create_list(:book, 3)}
+          before do 
+            get :index, params: {}
+          end
+
+          it 'responds with the book json' do
+            expected = ActiveModel::Serializer::CollectionSerializer.new(
+              books, each_serializer: BookSerializer
+            ).to_json
+
+            expect(response_body.to_json)
+          end
+        end
+      end
     end
   end
 end
