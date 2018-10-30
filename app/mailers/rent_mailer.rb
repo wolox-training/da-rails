9ba88684273b6c1604ae77cf771f5c0f1197ab1a
darwin araque <ingdarwinaraque@gmail.com>
2018-10-30 16:19:12 -0500

    modifications rents for rubocop

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,6 +1,5 @@
 class RentMailer < ApplicationMailer
-
-  def rent_created (rent)
+  def rent_created(rent)
     @rent = rent
     mail(to: @rent.user.email, subject: 'sending a confirm rent email')
   end
