c45594d2c64378804b102c81f515a417ddc58bd4
darwin araque <ingdarwinaraque@gmail.com>
2018-10-31 14:49:41 -0500

    This feature allows send a mail with the locale languaje setting in the user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_211125) do
+ActiveRecord::Schema.define(version: 2018_10_31_152444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_211125) do
     t.string "nickname"
     t.string "image"
     t.json "tokens"
+    t.string "locale"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
