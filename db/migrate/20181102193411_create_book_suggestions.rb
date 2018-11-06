669c44bcde14878e63ba17bdf78477b58b810311
darwin araque <ingdarwinaraque@gmail.com>
2018-11-06 15:30:27 -0500

    Corrention user can be null

--- db/migrate/20181102193411_create_book_suggestions.rb
+++ db/migrate/20181102193411_create_book_suggestions.rb
@@ -8,7 +8,7 @@ class CreateBookSuggestions < ActiveRecord::Migration[5.2]
       t.string :link, null: false
       t.string :editor, null: false 
       t.string :year, null: false
-      t.references :user, foreign_key: true, null: false
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
