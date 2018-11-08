e576399f29047793c9e6a9389df17424272035c6
darwin araque <ingdarwinaraque@gmail.com>
2018-11-08 12:54:09 -0500

    Sending modifications for good practices

--- spec/controllers/api/v1/book_suggestions_controller_spec.rb
+++ spec/controllers/api/v1/book_suggestions_controller_spec.rb
@@ -4,13 +4,13 @@ module Api
   module V1
     describe BookSuggestionsController, type: :controller do
       describe 'POST #create' do
-        context 'When the booksuggestions was created' do
+        context 'With the correct params incrementes the book suggestions counter by one,' do
           let!(:new_book_suggestions_attributes) do
             { book_suggestion: attributes_for(:book_suggestion) }
           end
           let(:request) { post :create, params: new_book_suggestions_attributes }
 
-          it 'It was created sucessfull' do
+          it 'increments the book suggestions counter by one' do
             expect { request }.to change { BookSuggestion.count }.by(1)
           end
 
@@ -20,17 +20,17 @@ module Api
           end
         end
 
-        context 'When creating an invalid book suggestion' do
+        context 'With the wrong params not incrementes the book suggestions' do
           let!(:new_book_suggestions_attributes) do
             { book_suggestion: attributes_for(:book_suggestion, price: nil) }
           end
           let(:request) { post :create, params: new_book_suggestions_attributes }
 
-          it 'It was not created because the price is nil' do
+          it 'not increments the book suggestions counter' do
             expect { request }.to change { BookSuggestion.count }.by(0)
           end
 
-          it 'returns error view' do
+          it 'returns unprocessable entity' do
             request
             expect(response).to have_http_status(:unprocessable_entity)
           end
