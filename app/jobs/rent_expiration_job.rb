ea6de2f86e491c754fa1ae244794748cebba90da
darwin araque <ingdarwinaraque@gmail.com>
2018-11-13 11:56:48 -0500

    modification date now fro time now

--- app/jobs/rent_expiration_job.rb
+++ app/jobs/rent_expiration_job.rb
@@ -1,7 +1,7 @@
 class RentExpirationJob
   include Sidekiq::Worker
   def perform
-    rents = Rent.where('end_date = ?', Time.zone.now)
+    rents = Rent.where('end_date = ?', Date.today)
     rents.each do |rent|
       RentMailer.rent_expire(rent).deliver_later
     end
