0eaac8bb754d1b6244e72cd228e4af43da48339f
darwin araque <ingdarwinaraque@gmail.com>
2018-11-13 11:38:19 -0500

    first commit to save this incomplete test

---
+++ spec/controllers/api/v1/rents_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+module Api
+  module V1
+    describe BooksController, type: :controller do
+      include_context 'Authenticated User'
+      describe 'POST #create' do
+        context 'With the correct params' do
+          let!(:new_rent_attributes) do
+            { rent: attributes_for(:rent, user_id: user.id) }
+          end
+          let(:request) { post :create, params: new_rent_attributes }
+
+          it 'Increments the Rent count by one' do
+            expect { request }.to change { Rent.count }.by(1)
+          end
+
+          it 'responds with 201 status' do
+            request
+            expect(response).to have_http_status(:created)
+          end
+        end
+      end
+    end
+  end
+end
